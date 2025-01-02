c0ef69853befcc7e3dfa38e1b9066c127fa14244
codrinursachi <codrinursachi@gmail.com>
2025-01-02 23:30:32 +0200

    add associations between models

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,3 +1,4 @@
 class Database < ApplicationRecord
   belongs_to :user
+  has_many :passwords, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
+  has_many :databases, dependent: :destroy
+  has_many :passwords, through: :databases
 end
