a3f35bbfdd95a8a41cd7155ea261edebe71beedf
codrinursachi <codrinursachi@gmail.com>
2025-01-02 22:08:57 +0200

    make login necessary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  before_action :authentication_required
+
+  def authentication_required
+    redirect_to login_path unless logged_in?
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
 end
