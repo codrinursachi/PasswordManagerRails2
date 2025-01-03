eeb2de92c4be0f3b3c9fa7f8a223242c954ec7a8
codrinursachi <codrinursachi@gmail.com>
2025-01-03 21:04:58 +0200

    additional fix for no db case

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      session[:database_id] = @user.databases.first.id
+      session[:database_id] = @user.databases.first.id if @user.databases.first
       redirect_to root_path
     else
       flash.now[:alert] = "Invalid username or password"
